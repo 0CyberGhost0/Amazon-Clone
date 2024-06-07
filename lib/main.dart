10502800eada31fc2d9bdf161ff6eb5b67f7da84
Ved <prakashved0702@gmail.com>
2024-06-07 09:29:25 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AmazonClone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariable.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariable.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+        useMaterial3: true, // can remove this line
+      ),
+      home: Scaffold(),
+    );
+  }
+}

0ff846aae9933cdfa331ab2254a68feeea902b89
Ved <prakashved0702@gmail.com>
2024-07-16 10:20:00 +0530

    Initial Commit

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose=require('mongoose');
 const authRouter=require('./routes/auth');
 const User=require('./models/user')
 const bcrypt=require('bcryptjs');
+const jwt=require("jsonwebtoken");
 const dbUrl="mongodb+srv://ved:admin@amazon.mw9rykz.mongodb.net/?retryWrites=true&w=majority&appName=amazon"
 mongoose.connect(dbUrl).then(()=>{
     console.log("Database Connected");}
@@ -11,29 +12,30 @@ mongoose.connect(dbUrl).then(()=>{
     console.log(e);
 });
 app.use(express.json());
-app.post('/api/signup',async (req,res)=>{
-    try {
-     console.log('Inside Signup operation');
-      const {name,email,password}=req.body;
-      const existingUser=await User.findOne({email});
-      if(existingUser){
-          res.status(400).json({error:"User with same email Already exists"});
-      }
-      const hashPassword=await bcrypt.hash(password,8);
-      let user=new User({
-          name,
-          password:hashPassword,
-          email,
-      });
-      user=await user.save();
-      res.json(user);
-    } catch (e) {
-     res.status(500).json({error:e.message});
+// app.post('/api/signup',async (req,res)=>{
+//     try {
+//      console.log('Inside Signup operation');
+//       const {name,email,password}=req.body;
+//       const existingUser=await User.findOne({email});
+//       if(existingUser){
+//           return res.status(400).json({error:"User with same email Already exists"});
+//       }
+//       const hashPassword=await bcrypt.hash(password,8);
+//       let user=new User({
+//           name,
+//           password:hashPassword,
+//           email,
+//       });
+//       user=await user.save();
+//       res.json(user);
+//     } catch (e) {
+//      res.status(500).json({error:e.message});
      
-    }
+//     }
  
- });
-// app.use('/',authRouter);
+//  });
+
+app.use('/',authRouter);
 app.listen(3000,()=>{
     console.log("Listening on Port 3000");
 });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,9 +2,29 @@ const express=require('express');
 const authRouter=express.Router();
 const User=require('../models/user');
 const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+authRouter.post('/api/signin',async(req,res)=>{
+    try {
+        console.log("Inside Signin in auth");
+        const {email,password}=req.body;
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({msg:"User doesn't Exist"});
+        }
+        const isMatch=await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({msg:"Incorrect Password"});
+        }
+        const token=jwt.sign({id:user._id},"password");
+        res.json({token,...user._doc});
+
+    } catch (error) {
+        res.status(500).json({error:error.message});
+        
+    }
+});
 authRouter.post('/api/signup',async (req,res)=>{
    try {
-    console.log('Inside Sigup operation');
      const {name,email,password}=req.body;
      const existingUser=await User.findOne({email});
      if(existingUser){
@@ -19,9 +39,9 @@ authRouter.post('/api/signup',async (req,res)=>{
      user=await user.save();
      res.json(user);
    } catch (e) {
-    res.status(500).json({error:e.message});
-    
+        res.status(500).json({error:e.message});
    }
 
 });
+
 module.exports=authRouter;
